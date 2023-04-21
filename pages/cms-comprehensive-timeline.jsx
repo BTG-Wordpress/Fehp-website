67e727bedd5f2eb9ed4e1eb7196f0f985edb62aa
Bran0612 <branchembahbolo@gmail.com>
2023-04-21 13:57:37 -0400

    Cms Comprehensive Timeline page added

---
+++ pages/cms-comprehensive-timeline.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+// components
+import { Layout } from "../src/components/index";
+
+import styles from "../styles/pages/cms-comprehensive-timeline.module.scss";
+// styles
+
+// ------------------------
+
+export default function CmsComprehensiveTimeline() {
+    return (
+        <Layout title='Cms Comprehensive Timeline'>
+            <section className={styles.wrap}>
+                <div className={styles.header}>
+                    <h1>CMS Comprehensive Timeline</h1>
+                </div>
+                <div className={styles.news_stories}>
+                    <div className={styles.articles}>
+                        <h2 style={{ textAlign: "center" }}>Established CMS Rules and Practice Acknowledge the Hold Harmless Prohibition Is Triggered Only By State Action</h2>
+                        <h3>1993</h3>
+                        <blockquote>
+                            <p>CMS responds to comments on a proposed rule relating to hold harmless agreements and <strong>
+                                <a
+                                    href="https://mcusercontent.com/6f30dcabca324361c55ad7aa4/files/829f40ab-8dac-8970-dd1c-67b1b73bfd92/1993_Health_Care_Financing_Administration_Final_Rule.pdf"
+                                    data-type="URL"
+                                    data-id="https://mcusercontent.com/6f30dcabca324361c55ad7aa4/files/829f40ab-8dac-8970-dd1c-67b1b73bfd92/1993_Health_Care_Financing_Administration_Final_Rule.pdf"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                >
+                                    focuses only on the State’s role in such agreements without ever mentioning private mitigation agreements
+                                </a>
+                            </strong>.
+                            </p>
+                        </blockquote>
+                        <h3>2003</h3>
+                        <blockquote>
+                            <p>An OIG audit of a Missouri provider assessment finds that <strong>
+                                <a
+                                    href="https://tehp.org/wp-content/uploads/2023/03/2003-MO-OIG-MHA-report.pdf"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                >
+                                    CMS has no authority to preclude private voluntary arrangements between providers
+                                </a>
+                            </strong>.
+                            </p>
+                        </blockquote>
+                        <h3>2004</h3>
+                        <blockquote>
+                            <p>Another OIG audit of a Missouri provider assessment <strong>
+                                <a
+                                    rel="noreferrer noopener"
+                                    href="https://tehp.org/wp-content/uploads/2023/03/2004-Review-MO-Provider-Tax-OIG-Recc-CMS-Agreement.pdf"
+                                    target="_blank"
+                                >
+                                    allows for private voluntary pooling arrangements between providers
+                                </a>
+                            </strong>. CMS adopts all of the OIG’s recommendations for reporting requirements for these voluntary pool disbursements. After this, CMS continues to allow the Missouri supplemental payments supported by the Missouri provider assessment with which the voluntary pool is associated.
+                            </p>
+                        </blockquote>
+                        <h3>2008</h3>
+                        <blockquote>
+                            <p>CMS responds to comments on a proposed rule relating to hold harmless agreements and <strong><a rel="noreferrer noopener" href="https://tehp.org/wp-content/uploads/2023/03/2008-Final-Rule.pdf" target="_blank">focuses only on the State’s role in such agreements without ever mentioning private mitigation agreements</a></strong>.</p>
+                            <p>In a hearing before the Provider Reimbursement Review Board, Patrick Cogley, a regional inspector general for the Office of Inspector General at Health and Human Services, discussed the details of the Missouri pooling arrangement. He said that while the arrangement was designed to mitigate, it did not constitute a hold harmless.<strong>&nbsp;</strong><a href="https://tehp.org/wp-content/uploads/2023/04/Kindred-Govt-Brief-Summary-with-audio-links.pdf" target="_blank" rel="noreferrer noopener"><strong>A Board member asked, “The question in my mind is, if one is looking at substance, does this arrangement violate the federal law with regard to Medicaid funding?” Cogley responded, “The answer is no.”</strong></a></p>
+                        </blockquote>
+                        <h3>2010</h3>
+                        <blockquote>
+                            <p>The California Hospital Association (CHA) released a presentation <a href="https://tehp.org/wp-content/uploads/2023/03/CA-Hospital-Provider-Fee-PPT-9.10.10.pdf" target="_blank" rel="noreferrer noopener"><strong>intimately detaining the CHFT Private Program, a financing vehichle used to coordinate private agreements among hospitals</strong> </a>surrounding redistribution.</p>
+                            <p>The Patient Protection and Affordable Care Act was signed into law by President Obama. The ACA reduced the amount individuals and families paid in uncompensated care and required every American to have health insurance.</p>
+                        </blockquote>
+                        <h3>2012</h3>
+                        <blockquote>
+                            <p><strong><a rel="noreferrer noopener" href="https://tehp.org/wp-content/uploads/2023/03/2012-Kindred-8th-Cir_.pdf" target="_blank">The Court of Appeals for the Eighth Circuit upheld CMS’s decisions about the proper cost reporting practices for Missouri’s pool payments</a></strong>&nbsp;in Kindred Hospitals East, LLC v. Sebelius, 694 F.3d 924 (8th Cir. 2012). In its brief, the government detailed its knowledge of the pooling arrangement purpose—to reduce the economic burden of Missouri’s provider tax on participating hospitals, and to redistribute Medicaid money from hospitals that treated many low-income and uninsured patients (“winner” hospitals) to “loser” hospitals that treated few low income and uninsured patients (and therefore received much less in supplemental payments). At oral argument, the government acknowledged that pooling arrangements existed around the country in “various iterations” for twenty years.</p>
+                        </blockquote>
+                        <h3>2018</h3>
+                        <blockquote>
+                            <p>HHS OIG conducted a review of seven States—California, Illinois, Indiana, Michigan, Missouri, Ohio, and Pennsylvania—to determine if hospital tax programs complied with hold-harmless requirements. <strong><a rel="noreferrer noopener" href="https://tehp.org/wp-content/uploads/2023/03/Although-Hospital-Tax-Programs-in-Seven-States-Complied-With-Hold-Harmless-Requirements-the-Tax-Burden-on-Hospitals-Was-Significantly-Mitigated-A-03-16-00202_compressed.pdf" target="_blank">OIG determined that taxes in these states were compliant because “[t]he States did not directly guarantee to hold hospitals harmless for the taxes, and hospital taxes did not exceed the threshold of 6 percent in the first prong of the indirect guarantee test.”</a></strong> Even though Missouri was included where private agreements were well-known, no mention was made of any prohibition on private agreements amongst providers.</p>
+                        </blockquote>
+                        <h3>2019</h3>
+                        <blockquote>
+                            <p><strong><a href="https://tehp.org/wp-content/uploads/2023/03/Kristin-Fan-Email-4.10.19.pdf" target="_blank" rel="noreferrer noopener">CMS confirms that it does not have statutory authority to address private mitigation agreements</a></strong> in response to an email from Barbara Eyman.</p>
+                            <p><strong><a rel="noreferrer noopener" href="https://tehp.org/wp-content/uploads/2023/03/MFAR-FULL-TEXT-RULE.pdf" target="_blank">CMS proposes the Medicaid Program; Medicaid Fiscal Accountability Regulation</a></strong>, 84 Fed. Reg. 63722 (Nov. 18, 2019) (MFAR), that would change the standard for determining whether a hold harmless exists. Language in the preamble indicated that CMS intended this revision to remove the requirement for State action.</p>
+                        </blockquote>
+                        <h3>2020</h3>
+                        <blockquote>
+                            <p><strong><a href="https://tehp.org/wp-content/uploads/2023/03/2023-14-States-MFAR-Chart.pdf" target="_blank" rel="noreferrer noopener">At least 14 states indicate that MFAR’s new definition of hold harmless would have a detrimental effect on their Medicaid programs</a></strong> if the existence of private agreements invalidated a provider tax.</p>
+                        </blockquote>
+                        <h3>2021</h3>
+                        <blockquote>
+                            <p><strong><a href="https://tehp.org/wp-content/uploads/2023/03/MFAR-Withdrawal-1.19.21.pdf" target="_blank" rel="noreferrer noopener">CMS withdrew the proposed rule after a broad array of stakeholders filed over 10,000 comments in opposition</a></strong>. CMS acknowledged that numerous commenters stated CMS “lacked statutory authority for its proposals and was creating regulatory provisions that were ambiguous or unclear and subject to excessive Agency discretion.”</p>
+                            <p>CMS approves preprints for multiple states that had previously indicated that MFAR’s new definition of hold harmless would have a detrimental effect on their Medicaid programs if the existence of private agreements invalidated a provider tax.</p>
+                        </blockquote>
+                        <h3>2022</h3>
+                        <blockquote>
+                            <p>In 2022, CMS initially withheld approval of Texas Medicaid directed payment programs because of “concerns” about possible redistribution arrangements, but CMS eventually approved the payment programs.</p>
+                            <p>In this process, a federal judge observed that CMS’s interpretive position—very similar to that set forth in the bulletin—<strong><a rel="noreferrer noopener" href="https://tehp.org/wp-content/uploads/2023/03/Barker-Opinion-3.11.22.pdf" data-type="URL" data-id="https://tehp.org/wp-content/uploads/2023/03/Barker-Opinion-3.11.22.pdf" target="_blank">was “distanced” from the text of the governing statute</a></strong>.</p>
+                            <p>Despite this loss in a federal court, CMS sent “focused audit” letters to several states, including Missouri, Texas, and Florida, indicating the agency planned to <strong><a rel="noreferrer noopener" href="https://tehp.org/wp-content/uploads/2023/03/FL-TX-Audits.pdf" target="_blank">review state Medicaid financing plans for compliance with the expanded hold harmless definition first announced in MFAR</a></strong>.</p>
+                        </blockquote>
+                        <h3>2023</h3>
+                        <blockquote>
+                            <p>On February 17, <strong><a rel="noreferrer noopener" href="https://tehp.org/wp-content/uploads/2023/03/Informational-Bulletin-2.17.23.pdf" target="_blank">CMS issued an Informational Bulletin providing new guidance on health care related taxes and hold harmless arrangements</a></strong>, specifically arrangements involving the redistribution of Medicaid payments.</p>
+                            <p><strong><a href="https://tehp.org/wp-content/uploads/2023/04/1.1.pdf" target="_blank" rel="noreferrer noopener">The State of Texas filed a complaint against CMS over the Informational Bulletin released on February 17, 2023.</a></strong>&nbsp;Texas contends that the bulletin is contrary to law and that the bulletin’s policies, if implemented, “will have an immediate impact on not just [Texas’s] ability to provide vitally needed healthcare services to Texans but also on Texas’s sovereign interest in enforcing its laws.”</p>
+                        </blockquote>
+                    </div>
+                </div>
+            </section>
+            <div className={styles.footer_image}></div>
+        </Layout>
+    );
+}
